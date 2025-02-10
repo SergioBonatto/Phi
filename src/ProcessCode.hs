312cf6ce5950a8393f1ba3bbc8c855c61c57818a
SergioBonatto <bonatto@tutanota.com>
2025-02-10 12:07:00 -0300

    refactor: ProcessCode

--- src/ProcessCode.hs
+++ src/ProcessCode.hs
@@ -8,24 +8,33 @@ import ParseDefinition (parseDefinition)
 import ParseCommon (parseExpr)
 import ParseError (ParseError(..))
 
+stripLine :: String -> String
+stripLine = unwords . words
+
+processSingleLine :: [String] -> Env -> Maybe Expression -> Either ParseError (Expression, Env, Maybe Expression)
+processSingleLine [] env lastExpr = case lastExpr of
+    Nothing -> Left EmptyExpression
+    Just e -> Right (e, env, Just e)
+processSingleLine tokens env lastExpr = case tokens of
+    [] -> Right (undefined, env, lastExpr)
+    ("let":_) -> do
+        (name, expr) <- parseDefinition tokens
+        let env' = Map.insert name expr env
+        Right (expr, env', Just expr)
+    _ -> do
+        (expr, _) <- parseExpr tokens
+        Right (expr, env, Just expr)
+
+processLine :: Either ParseError (Expression, Env, Maybe Expression) -> String -> Either ParseError (Expression, Env, Maybe Expression)
+processLine acc line = do
+    (_, env, lastExpr) <- acc
+    let tokens = tokenize line
+    processSingleLine tokens env lastExpr
+
 processCode :: String -> Either ParseError (Expression, Env)
 processCode code =
-    let ls = filter (not . null) $ map strip $ lines code
-    in go ls Map.empty Nothing
-  where
-    go [] env lastExpr = case lastExpr of
-                           Nothing -> Left EmptyExpression
-                           Just e  -> Right (e, env)
-    go (l:ls') env lastExpr =
-        let tokens = tokenize l
-        in case tokens of
-             [] -> go ls' env lastExpr
-             ("let":_) -> do
-                 (name, expr) <- parseDefinition tokens
-                 let env' = Map.insert name expr env
-                 go ls' env' (Just expr)
-             _ -> do
-                 (expr, _) <- parseExpr tokens
-                 go ls' env (Just expr)
-
-    strip = unwords . words
+    let lines' = filter (not . null) $ map stripLine $ lines code
+        result = foldl processLine (Right (undefined, Map.empty, Nothing)) lines'
+    in case result of
+        Left err -> Left err
+        Right (expr, env, _) -> Right (expr, env)
