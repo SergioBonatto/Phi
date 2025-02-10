160846a6bb8ad1966b1fcd42974a9d63d3396a18
SergioBonatto <bonatto@tutanota.com>
2025-02-10 19:07:09 -0300

    update: Evaluator

--- src/Core/Evaluator.hs
+++ src/Core/Evaluator.hs
@@ -6,4 +6,4 @@ import Data.Set (Set)
 import EvaluateStep (evaluateStep)
 
 evaluate :: Expression -> Env -> Set String -> Int -> (Expression, Int)
-evaluate expr env _usedDefs maxSteps = evaluateStep expr env 0 maxSteps
+evaluate expr env _usedDefs = evaluateStep expr env 0
