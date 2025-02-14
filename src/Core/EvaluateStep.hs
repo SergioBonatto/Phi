7e99e069fd1539db6465baaaba50de4c744cfc76
SergioBonatto <bonatto@tutanota.com>
2025-02-14 00:51:26 -0300

    fix: EvaluatorStep

--- src/Core/EvaluateStep.hs
+++ src/Core/EvaluateStep.hs
@@ -6,7 +6,7 @@ import Substitution (substitute)
 import qualified Data.Map as Map
 import Data.Set (Set)
 import Types (InterpreterConfig(..), EvalTrace(..), MemoTable, EvalResult)
-import Error (Error(..))
+import Error()
 
 evaluateStep :: InterpreterConfig
              -> Expression
@@ -19,6 +19,7 @@ evaluateStep :: InterpreterConfig
 evaluateStep config e env usedDefs steps memo traces
     | steps >= maxSteps config = error "Maximum number of reduction steps exceeded"
     | otherwise = case e of
+        -- Caso para variáveis
         Var x -> case Map.lookup x env of
             Just ex ->
                 let trace = EvalTrace {
@@ -31,3 +32,26 @@ evaluateStep config e env usedDefs steps memo traces
                     (result, s, ts, m) = evaluateStep config ex env usedDefs (steps + 1) memo (trace:traces)
                 in (result, s, ts, m)
             Nothing -> (e, steps, traces, memo)
+
+        -- Caso para aplicações
+        App e1 e2 ->
+            let (e1', s1, t1, m1) = evaluateStep config e1 env usedDefs steps memo traces
+            in case e1' of
+                Lam x body ->
+                    let trace = EvalTrace {
+                            step = s1,
+                            expr = e,
+                            redex = Just "Beta reduction",
+                            memoryUsage = Map.size m1,
+                            optimization = Nothing
+                        }
+                        reduced = substitute body x e2
+                    in evaluateStep config reduced env usedDefs (s1 + 1) m1 (trace:t1)
+                _ ->
+                    let (e2', s2, t2, m2) = evaluateStep config e2 env usedDefs s1 m1 t1
+                    in (App e1' e2', s2, t2, m2)
+
+        -- Caso para lambda
+        Lam x body ->
+            let (body', s, t, m) = evaluateStep config body env usedDefs steps memo traces
+            in (Lam x body', s, t, m)
