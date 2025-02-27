165319a9a0e1a499e76d0d76be6c7679e494ba8b
SergioBonatto <bonatto@tutanota.com>
2025-02-27 04:35:54 -0300

    fix: evaluator

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -26,16 +26,24 @@ evaluate config expr =
            else result
 
 eval :: Expression -> EvalM Expression
-eval (Var x) = do
-    env <- gets envState
-    case Map.lookup x env of
-        Just e  -> return e
-        Nothing -> throwError $ UnboundVariable x
-eval (Lam x body) = return $ Lam x body
-eval (App f arg) = do
-    f' <- eval f
-    case f' of
-        Lam x body -> do
-            arg' <- eval arg
-            local (Map.insert x arg') (eval body)
-        _ -> throwError $ TypeError "Expected a function in application"
+eval expr = do
+    incrementStep
+    addTrace "Evaluating" expr
+    case expr of
+        Var x -> do
+            env <- gets envState
+            case Map.lookup x env of
+                Just e  -> eval e
+                Nothing -> return (Var x)
+        Lam x body -> return $ Lam x body
+        App f arg -> do
+            f' <- eval f
+            case f' of
+                Lam x body -> do
+                    arg' <- eval arg
+                    let newBody = substitute x arg' body
+                    addTrace ("Substituting " ++ x) newBody
+                    eval newBody
+                _ -> do
+                    arg' <- eval arg
+                    return $ App f' arg'
